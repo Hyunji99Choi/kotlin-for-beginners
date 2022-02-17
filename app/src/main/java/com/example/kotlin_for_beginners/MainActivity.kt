c325d421311d70689ba1749a7c33eb3cb4ec7bc8
Hyunji99Choi <tyg06229@gmail.com>
2022-02-17 21:14:20 +0900

    뷰 바인딩

--- app/src/main/java/com/example/kotlin_for_beginners/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_for_beginners/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.kotlin_for_beginners
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.kotlin_for_beginners.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        //어떻게 메인액티비티와 연결되는가,, 전부 연결되는거 아님?
+        setContentView(binding.root)
+
+
+        //뷰바인딩 변수.id
+        binding.btnSay.setOnClickListener {
+            binding.textSay.text = "Hello Kotlin!!"
+        }
+
     }
 }
